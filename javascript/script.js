4453172f1a1a154c91dd3597c0e5170b19d21153
Braydon <braydonpettit24@outlook.com>
2024-11-03 20:09:20 +1100

    Added product page layout, javascript and styling - Added product page layout including JavaScript logic and styling - Added smoother hover effect on navbar links - Changed hover effect on the terms page

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,14 @@
-$(function() {
+$(window).on('load', function() {
     // Navbar toggle
-    $('#navbar-toggle').on('click', function() {
-        $('#navbar-menu').slideToggle();
+    $('.navbar-toggle').on('click', function() {
+        $('.navbar-menu').slideToggle();
     });
 
     $(window).on('resize', function() {
         if ($(window).width() > 430) {
-            $('#navbar-menu').show();
+            $('.navbar-menu').show();
         } else {
-            $('#navbar-menu').hide();
+            $('.navbar-menu').hide();
         }
     }).trigger('resize');
 
@@ -38,8 +38,74 @@ $(function() {
         $('.dropdown-content').not($(this).next('.dropdown-content')).slideUp();
         $('.btn i').not($(this).find('i')).removeClass('open').addClass('closed');
         
+        // Remove the open class from all buttons except the one being clicked
+        $('.btn').not(this).removeClass('open');
+        
         // Toggle the clicked dropdown
         $(this).next('.dropdown-content').slideToggle();
         $(this).find('i').toggleClass('closed open');
+        
+        // Toggle the open class on the button
+        $(this).toggleClass('open');
+    });
+});
+
+// Function to get URL parameters
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    const results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+// Function to update product details
+function updateProductDetails() {
+    const productName = getUrlParameter('name');
+    const productPrice = getUrlParameter('price');
+
+    const productNameElement = document.getElementById('productName');
+    const productPriceElement = document.getElementById('productPrice');
+
+    if (productName && productNameElement) {
+        productNameElement.textContent = productName;
+    }
+    if (productPrice && productPriceElement) {
+        productPriceElement.textContent = `$${productPrice}`;
+    }
+}
+
+// Call the function to update product details on page load
+document.addEventListener('DOMContentLoaded', updateProductDetails);
+
+
+// Global variable to keep track of the current image index
+let currentImageIndex = 0;
+const images = ['front', 'back'];
+
+//
+function showImage(imageType) {
+    const fullImage = document.getElementById('fullImage');
+    const fullImageLink = document.getElementById('fullImageLink');
+    if (imageType === 'front') {
+        fullImage.src = '../../../images/front.png';
+        fullImageLink.href = '../../../images/front.png';
+        currentImageIndex = 0;
+    } else if (imageType === 'back') {
+        fullImage.src = '../../../images/back.png';
+        fullImageLink.href = '../../../images/back.png';
+        currentImageIndex = 1;
+    }
+}
+
+// Magnific Popup for Product Images
+$(function() {
+    $('.full-image a').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: false
+        },
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        showCloseBtn: false
     });
 });
